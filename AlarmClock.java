885bb51424686dd9ce1a5c515d2852e7edcb3fc1
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 17:07:02 +0545

    Add AlarmClock class to implement a simple alarm time setter

---
+++ AlarmClock.java
@@ -0,0 +1,27 @@
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class AlarmClock {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = sc.nextLine();
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for: " + alarmTime);
+            } 
+            catch (DateTimeParseException e) {
+                System.out.println("Invalid format. Please use HH:MM:SS");
+            }
+        }
+        sc.close();
+    }
+}
