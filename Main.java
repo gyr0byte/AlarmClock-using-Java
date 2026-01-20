ac411fc0287387254c2a7b45b64e5d03f5c30318
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 17:08:44 +0545

    Add Main class to implement alarm time setting functionality

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
-public class AlarmClock {
+public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
